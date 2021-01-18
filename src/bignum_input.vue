0c94a41eb94a83f2c7eecde14cd41146c2dbf4a8
Emre Kanatli <kanatliemre@gmail.com>
2021-01-18 15:32:13 -0500

    Upgrade big.js dependency Update BignumInput to use vanilla html input number element

--- src/bignum_input.vue
+++ src/bignum_input.vue
@@ -1,29 +1,14 @@
 <template>
-    <CurrencyInput :allow-negative="false"
-                    v-model="val"
-                    locale="default"
-                    :currency="null"
-                    :value-as-integer="false"
-                    :precision="denomination"
-                    :auto-decimal-mode="true"
-                    :value-range="range"
-                   ref="curIn"
-                   @keydown.up.native="keyUp"
-                   @keydown.down.native="keyDown"
-    ></CurrencyInput>
+    <input type="number" :placeholder="placeholder" v-model="val" :min="min" :max="maxNum" :step="stepNum" @change="onChange">
 </template>
 <script>
     import Big from 'big.js';
     import * as BN from 'bn.js';
-    import {CurrencyInput} from 'vue-currency-input';
 
     export default {
-        components:{
-            CurrencyInput
-        },
         data(){
             return {
-                val: 0,
+                val: 0.00,
             }
         },
         computed: {
@@ -38,6 +23,27 @@
                     min: this.min.toString(),
                     max: maxStr
                 }
+            },
+            maxNum(){
+                if(!this.step) return null
+                try{
+                    let pow = (new Big(this.max.toString())).div(Math.pow(10,this.denomination))
+                    return pow.toNumber()
+                }catch (e){
+                    console.error(e)
+                    return null
+                }
+            },
+            stepNum(){
+                if(!this.step) return 0.01
+                try{
+                    let pow = (new Big(this.step.toString())).div(Math.pow(10,this.denomination))
+                    return pow.toNumber()
+                }catch (e){
+                    console.error(e)
+                    return 0.01
+                }
+
             }
         },
         props: {
@@ -65,33 +71,46 @@
         },
         watch: {
             val(val){
-                let mult = Math.pow(10,this.denomination);
-                let valBig = Big(val).times(mult);
-                let strVal = valBig.toString();
-                let bnVal = new BN(strVal);
-                this.$emit('change', bnVal);
+
+                //
+                try{
+                    let splitVal = val.toString().split('.')
+                    let wholeVal = splitVal[0]
+                    let denomVal = splitVal[1]
+                    if(denomVal){
+                        if(denomVal.length > this.denomination){
+                            let newDenom = denomVal.substring(0,this.denomination)
+                            this.val = parseFloat(`${wholeVal}.${newDenom}`)
+                            return
+                        }
+                    }
+                }catch (e){
+                    console.log(e)
+                }
+
+                if(!val){
+                    this.$emit('change', new BN(0));
+                    return
+                }
+                let valEdit = (parseFloat(val).toFixed(this.denomination)).split('.').join('')
+                let valBn = new BN(valEdit)
+                this.$emit('change', valBn);
             }
         },
         methods: {
-            keyUp(ev){
-                ev.preventDefault();
-                let dif = Big(1).div(Math.pow(10,this.denomination))
-                let newVal = Big(this.val).add(dif);
-                this.$refs.curIn.setValue(newVal.toString());
-            },
-            keyDown(ev){
-                ev.preventDefault();
-                let dif = Big(1).div(Math.pow(10,this.denomination))
-                let newVal = Big(this.val).sub(dif);
-                this.$refs.curIn.setValue(newVal.toString());
-            },
             maxout(){
-                if(this.max){
-                    this.$refs.curIn.setValue(this.max.toString());
+                if(this.maxNum){
+                    this.val = this.maxNum
                 }
             },
             clear(){
-                this.$refs.curIn.setValue('0');
+                this.val = 0
+            },
+            onChange(){
+                // If number is above max amount, correct it
+                if(this.val > this.maxNum){
+                    this.val = this.maxNum
+                }
             }
         }
     }
@@ -99,5 +118,17 @@
 <style scoped>
     input{
         text-align: right;
+        outline: none;
+    }
+    /* Chrome, Safari, Edge, Opera */
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    /* Firefox */
+    input[type=number] {
+        -moz-appearance: textfield;
     }
 </style>
