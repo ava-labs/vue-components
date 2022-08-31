844f96fd94368030c67ce67e4ec846d634db16c3
aeddaqqa <achrafeddaqqaq@gmail.com>
2022-08-31 10:35:02 +0100

    update bignum_input component

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,9 +1,9 @@
 // This file is used when building the NPM Module
 // Simply imports and exports hte components
 
-import QrReader from './qr_reader';
-import QrInput from './qr_input';
-import BigNumInput from './bignum_input';
+import QrReader from "./qr_reader";
+import QrInput from "./qr_input";
+import BigNumInput from "./bignum_input";
 import CopyText from "./CopyText";
 
-export {QrInput, QrReader, BigNumInput, CopyText};
+export { QrInput, QrReader, BigNumInput, CopyText };
